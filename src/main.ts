d94f0544c5d9319def405223b216c70d95dd7f21
Tania <tanialibramg@gmail.com>
2025-03-18 11:06:59 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import store from './store'
 
 import mapboxgl from 'mapbox-gl'; 
 
-mapboxgl.accessToken = 'pk.eyJ1IjoicHJnM3RpY25tZWRpYSIsImEiOiJjamhyZGcwZjIycTVmMzZxazl6cWxweTNxIn0.0ryMnkWLZHGQKlX8-J7J5Q';
+mapboxgl.accessToken = '';
 
 if(!navigator.geolocation){
     throw new Error("Tu navegador no soporta el Geolocation");    
